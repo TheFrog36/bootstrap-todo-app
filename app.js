597862076a9ed5eb05c486aa20d6a0d0518ec68a
Frog <artisangiorgio98@gmail.com>
2022-05-30 20:34:16 +0200

    asdaddsdadsds

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ let todosArray = [];
 
 
 function goToTodoPage(id) {
-  let urlString = "/todo.html"
-  if(id){
-    urlString = urlString + '?id=' + id;
-  }
-  window.location.href = urlString;
+    let urlString = "/todo.html"
+    if (id) {
+        urlString = urlString + '?id=' + id;
+    }
+    window.location.href = urlString;
 }
 
 // function goToTodoPage2(todo){
@@ -21,172 +21,164 @@ function goToTodoPage(id) {
 //   window.location.href = urlString;
 // }
 
-function populateTagContainer(container, tags){
-  for (const tag of tags) {
-    const span = document.createElement('span');
-    span.classList.add('chip');
-    const node = document.createTextNode('#' + tag);
-    span.appendChild(node);
-    container.appendChild(span)
-  }
+function populateTagContainer(container, tags) {
+    for (const tag of tags) {
+        const span = document.createElement('span');
+        span.classList.add('badge', 'rounded-pill', 'text-bg-dark');
+        const node = document.createTextNode('#' + tag);
+        span.appendChild(node);
+        container.appendChild(span)
+    }
 }
 
 
-function createTodoCard(todo){
+function createTodoCard(todo) {
 
-  const cardTemplate = `
-    <div class="card-header bg-transparent border-success">#NAME</div>
-    <div class="card-body text-success">
-        <div class="tag-container"></tag>
-        
-    </div>
-    <div class="card-footer bg-transparent border-success">#CREATIONDATE</div>`
-  
-  
-  //const humanDate = new Date(todo.creationDate * 1000)
-  const todoHtml = cardTemplate.replace('#NAME', todo.name)
-                               .replace('#CREATIONDATE', todo.creationDate.toLocaleString())
+    const cardTemplate = `
+        <div class="card-header bg-transparent border-dark  d-flex justify-content-between align-items-center">
+            #NAME
+            <div class="priority-ball" style="background-color: #PRIORITY_COLOR;"></div>
+        </div>
+        <div class="card-body text-dark " id="tags-container"> </div>
+        <div class="card-footer bg-transparent border-dark ">
+            <div class="d-flex justify-content-between align-items-center">
+                #CREATIONDATE
 
-  return todoHtml;
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <button id="delete-button" type="button" class="btn btn-outline-dark "><img src="./assets/delete.svg" height="20"></button>
+                    <button id="edit-button" type="button" class="btn btn-outline-dark "><img src="./assets/edit.svg" height="20"></button>
+                    <button id="done-button" type="button" class="btn btn-outline-dark "><img src="./assets/check.svg" height="20"></button>
+                </div>
+            </div>
+        </div>`
+
+
+    const todoHtml = cardTemplate.replace('#NAME', todo.name)
+        .replace('#CREATIONDATE', todo.creationDate.toLocaleString())
+        .replace('#PRIORITY_COLOR', todo.priority.color)
+
+    return todoHtml;
 }
 
-function startLoading(){
-//   const loader = document.getElementById('loader')
-//   loader.style.display = 'inline-block'
-//   const refresh = document.getElementById('refresh-button');
-//   refresh.style.display = 'none';
+function startLoading() {
+    const loader = document.getElementById('loader')
+    loader.style.display = 'inline-block'
+    const refresh = document.getElementById('refresh-button');
+    refresh.style.display = 'none';
 }
 
 function stopLoading() {
-//   const loader = document.getElementById('loader')
-//   loader.style.display = 'none'
-//   const refresh = document.getElementById('refresh-button');
-//   refresh.style.display = 'inline-block';
+    const loader = document.getElementById('loader')
+    loader.style.display = 'none'
+    const refresh = document.getElementById('refresh-button');
+    refresh.style.display = 'inline-block';
 }
 
-function filterTodos(t1, t2){
-  return t1.id !== t2.id;
+function filterTodos(t1, t2) {
+    return t1.id !== t2.id;
 }
 
-function removeTodoAndRefesh(todo){
-  stopLoading()
-  todosArray = todosArray.filter(t1 => filterTodos(t1, todo))
-  displayTodos(todosArray);
+function removeTodoAndRefesh(todo) {
+    stopLoading()
+    todosArray = todosArray.filter(t1 => filterTodos(t1, todo))
+    displayTodos(todosArray);
 }
 
-function deleteTodo(id){
-  
+function deleteTodo(id) {
+
     startLoading()
     const deleteUrl = BASE_URL + '/' + id;
     const fetchOptions = { method: 'delete' };
     fetch(deleteUrl, fetchOptions)
-      .then(response => response.json())
-      .then(result => removeTodoAndRefesh(result))
-      .catch(error => stopLoading())
-    
+        .then(response => response.json())
+        .then(result => removeTodoAndRefesh(result))
+        .catch(error => stopLoading())
+
 }
 
-function requestConfirmToDelete(id){
-  if (confirm('sei pazzo??? potresti pentirtene in futuro...')) {
-    deleteTodo(id)
-  } else {
-    alert('fiu... ci è mancato poco!')
-  }
-}
-
-function todoDone(todo){
-  todo.doneDate = new Date().getTime() / 1000;
-  todo.priority = Todo.PRIORITY.done;
-  const dbObj = todo.toDbObj();
-  const dbObjJson = JSON.stringify(dbObj);
-
-  const url = BASE_URL + '/' + todo.id;
-
-  fetchOptions = {
-    method: 'PUT', body: dbObjJson, headers: {
-      'Content-Type': 'application/json'
+function requestConfirmToDelete(id) {
+    if (confirm('sei pazzo??? potresti pentirtene in futuro...')) {
+        deleteTodo(id)
+    } else {
+        alert('fiu... ci è mancato poco!')
     }
-  };
+}
 
-  fetch(url, fetchOptions)
-  .then(resp => resp.json())
-  .then(res => displayTodos(todosArray))
+function todoDone(todo) {
+    todo.doneDate = new Date().getTime() / 1000;
+    todo.priority = Todo.PRIORITY.done;
+    const dbObj = todo.toDbObj();
+    const dbObjJson = JSON.stringify(dbObj);
+
+    const url = BASE_URL + '/' + todo.id;
+
+    fetchOptions = {
+        method: 'PUT', body: dbObjJson, headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    fetch(url, fetchOptions)
+        .then(resp => resp.json())
+        .then(res => displayTodos(todosArray))
 
 }
 
-function displayTodos(todos){
+function displayTodos(todos) {
 
-  todosArray.sort(Todo.orderTodoByPriority);
-  const todosContainer = document.getElementById('todos-container');
+    todosArray.sort(Todo.orderTodoByPriority);
+    const todosContainer = document.getElementById('todos-container');
 
-  todosContainer.innerHTML = '';
+    todosContainer.innerHTML = '';
 
-  for (const todo of todos) {
+    for (const todo of todos) {
 
-    const todoCard = document.createElement('div');
-    todoCard.classList.add('card', 'border-success', 'mb-3');
+        const todoCard = document.createElement('div');
+        todoCard.classList.add('card', 'border-dark', 'mb-3');
 
-    todoCard.innerHTML = createTodoCard(todo);
+        todoCard.innerHTML = createTodoCard(todo);
 
-    const tagContainer = todoCard.querySelector('.tag-container');
+        const tagContainer = todoCard.querySelector('#tags-container');
 
-    populateTagContainer(tagContainer, todo.tags)
+        populateTagContainer(tagContainer, todo.tags)
+        const deleteButton = todoCard.querySelector('#delete-button');
+        deleteButton.onclick = () => requestConfirmToDelete(todo.id);
 
-    // const deleteButton = todoCard.querySelector('.delete-button');
-    // deleteButton.onclick = () => requestConfirmToDelete(todo.id);
+        const editButton = todoCard.querySelector('#edit-button');
+        if (todo.doneDate) {
+          editButton.style.display = 'none';
+        } else {
+          editButton.onclick = () => goToTodoPage(todo.id);
+        }
 
-    // const editButton = todoCard.querySelector('.edit-button');
-    // if (todo.doneDate) {
-    //   editButton.style.display = 'none';
-    // } else {
-    //   editButton.onclick = () => goToTodoPage(todo.id);
-    // }
+
+        const doneButton = todoCard.querySelector('#done-button');
+        if (todo.doneDate) {
+          doneButton.style.display = 'none';
+        } else {
+          doneButton.onclick = () => todoDone(todo);
+        }
     
+        todosContainer.appendChild(todoCard);
 
-    // const doneButton = todoCard.querySelector('.done-button');
-    // if (todo.doneDate) {
-    //   doneButton.style.display = 'none';
-    // } else {
-    //   doneButton.onclick = () => todoDone(todo);
-    // }
-    
-  
-    // const divider = todoCard.querySelector('.divider');
-    // divider.style.backgroundColor = todo.priority.color;
+    }
 
-    // const span = document.createElement('span');
-    // const nameNode = document.createTextNode(todo.name);
-    // span.appendChild(nameNode);
-
-    // todoCard.appendChild(span);
-
-    // const button = document.createElement('button');
-    // button.onclick = () => deleteTodo(todo.id)
-    // const deleteNode = document.createTextNode('delete');
-    // button.appendChild(deleteNode);
-
-    // todoCard.appendChild(button);
-
-    todosContainer.appendChild(todoCard);
-
-  }
-  
 }
 
-function initTodos(todos){
-  stopLoading();
-  todosArray = todos.map(obj => Todo.fromDbObj(obj));
-  displayTodos(todosArray);
+function initTodos(todos) {
+    stopLoading();
+    todosArray = todos.map(obj => Todo.fromDbObj(obj));
+    displayTodos(todosArray);
 }
 
 
 
-function loadTodos(){
-  startLoading()
-  fetch(BASE_URL)
-  .then(response => response.json())
-  .then(result => initTodos(result))
-  //.catch(error => stopLoading())
+function loadTodos() {
+    startLoading()
+    fetch(BASE_URL)
+        .then(response => response.json())
+        .then(result => initTodos(result))
+    //.catch(error => stopLoading())
 }
 
 loadTodos()
